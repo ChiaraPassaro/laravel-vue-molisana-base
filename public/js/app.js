cb300b32013641f1d835813ab5b4ad4f3b1c586f
Chiara Passaro <chiarapassaro.wd@gmail.com>
2021-02-05 10:41:37 +0100

    js

--- public/js/app.js
+++ public/js/app.js
@@ -49900,8 +49900,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/kja/Siti/laravel-vue-molisana/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/kja/Siti/laravel-vue-molisana/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/kja/Siti/laravel-vue-molisana-base/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/kja/Siti/laravel-vue-molisana-base/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
